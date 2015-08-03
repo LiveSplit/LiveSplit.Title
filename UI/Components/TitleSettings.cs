cfe09eef450c8db7cc98fa4729d7dca79b4b31d3
Lioncash <mathew1800@gmail.com>
2015-08-02 21:20:40 -0400

    Remove unused project references and directives

--- UI/Components/TitleSettings.cs
+++ UI/Components/TitleSettings.cs
@@ -1,16 +1,5 @@
-﻿using Fetze.WinFormsColor;
-using LiveSplit.Options;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System;
 using System.Drawing;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 
