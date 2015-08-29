a9a13a3ac96ad2cc63c91ff327f120fdc31bfce6
Sergey Papushin <spapushin@gmail.com>
2015-08-29 14:47:55 -0500

    Update Component Version to 1.6

--- UI/Components/TitleFactory.cs
+++ UI/Components/TitleFactory.cs
@@ -50,7 +50,7 @@ namespace LiveSplit.UI.Components
 
         public Version Version
         {
-            get { return Version.Parse("1.5.2"); }
+            get { return Version.Parse("1.6"); }
         }
     }
 }
