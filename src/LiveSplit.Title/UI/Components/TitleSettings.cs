fd7007f23a5755a7b295e97b3de82811ea3ffbf2
Ero <imjustero@gmail.com>
2024-09-02 10:12:56 +0200

    Add and order modifiers
    * `IDE0036`, `IDE0040`, `IDE0044`, `IDE0064`, `IDE0250`, `IDE0251`

--- src/LiveSplit.Title/UI/Components/TitleSettings.cs
+++ src/LiveSplit.Title/UI/Components/TitleSettings.cs
@@ -76,24 +76,24 @@ public partial class TitleSettings : UserControl
         chkVariables.DataBindings.Add("Checked", this, "ShowVariables", false, DataSourceUpdateMode.OnPropertyChanged);
     }
 
-    void TitleSettings_Load(object sender, EventArgs e)
+    private void TitleSettings_Load(object sender, EventArgs e)
     {
         chkColor_CheckedChanged(null, null);
         chkFont_CheckedChanged(null, null);
         cmbTextAlignment.SelectedIndex = (int)TextAlignment;
     }
 
-    void chkColor_CheckedChanged(object sender, EventArgs e)
+    private void chkColor_CheckedChanged(object sender, EventArgs e)
     {
         label3.Enabled = btnColor.Enabled = chkColor.Checked;
     }
 
-    void chkFont_CheckedChanged(object sender, EventArgs e)
+    private void chkFont_CheckedChanged(object sender, EventArgs e)
     {
         label1.Enabled = lblFont.Enabled = btnFont.Enabled = chkFont.Checked;
     }
 
-    void cmbGradientType_SelectedIndexChanged(object sender, EventArgs e)
+    private void cmbGradientType_SelectedIndexChanged(object sender, EventArgs e)
     {
         btnColor1.Visible = cmbGradientType.SelectedItem.ToString() != "Plain";
         btnColor2.DataBindings.Clear();
@@ -101,7 +101,7 @@ public partial class TitleSettings : UserControl
         GradientString = cmbGradientType.SelectedItem.ToString();
     }
 
-    void cmbTextAlignment_SelectedIndexChanged(object sender, EventArgs e)
+    private void cmbTextAlignment_SelectedIndexChanged(object sender, EventArgs e)
     {
         TextAlignment = (AlignmentType)cmbTextAlignment.SelectedIndex;
     }
